af5cfc529728f082ff736520562998c09144a3ec
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-02-16 10:37:53 +0100

    [UPDATE] view on screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,12 +16,61 @@
 </head>
 
     <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Treni in partenza oggi</h1>
+                </div>
+                <div class="col-12">
 
-        @foreach ($trains as $train)
-            @if ($train->departure_time == '2024-02-15')
-                {{$train}}
-            @endif
-        @endforeach
+                    <table class="table table-striped m-5">
+                        <thead>
+                            <tr>
+                                <th>Azienda</th>
+                                <th>Stazione di partenza</th>
+                                <th>Stazione di arrivo</th>
+                                <th>Orario di partenza</th>
+                                <th>Orario di arrivo</th>
+                                <th>Codice</th>
+                                <th>Carrozze</th>
+                                <th>Ritardo</th>
+                                <th>Cancellato</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($trains as $train)
+                                @if ($train->departure_time == '2024-02-15')
+                            <tr>
+                                <td class="p-2">{{$train->factory}}</td>
+                                <td class="p-2">{{$train->departure_station}}</td>
+                                <td class="p-2">{{$train->arrival_station}}</td>
+                                <td class="p-2">{{$train->departure_time}}</td>
+                                <td class="p-2">{{$train->arrival_time}}</td>
+                                <td class="p-2">{{$train->train_code}}</td>
+                                <td class="p-2">{{$train->n_train_cars}}</td>
+                                <td class="p-2">
+                                    @if ($train->delay == 1)
+                                        Si
+                                    @else
+                                        No
+                                    @endif
+                                </td>
+                                <td class="p-2">
+                                    @if ($train->deleted == 1)
+                                    Si
+                                @else
+                                    No
+                                @endif
+                            </td>
+                            </tr>
+                            @endif
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        
 
     </body>
 
